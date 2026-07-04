abe58bf47a057c324da8a5e8310939ee9f0d6be5
smimran-alt <smimran@gmail.com>
2026-07-04 01:15:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.QUIZ_CONFIG = {
   quizTitle: "Shayan Grade 8 Math Quiz",
   defaultStudentName: "Shayan",
   parentEmail: "smimran@gmail.com",
-  web3formsAccessKey: ""
+  web3formsAccessKey: "1e4df947-ec4a-435e-bc3b-9928bf3fb77e"
 };
